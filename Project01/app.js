4cdf13cc2cb02e3b99f5b5ecd691de8a8cbd9249
bzchacal <bzchacal@gmail.com>
2018-04-08 23:27:47 +0100

    Project01

---
+++ Project01/app.js
@@ -0,0 +1,9 @@
+
+function putText () {
+  // document.getElementById("test").innerHTML = 'HI nizar'
+  document.querySelector('.calss_').innerHTML = 'hello calss'
+  document.querySelector('#id_').innerHTML = 'hello idd'
+  let a = 5
+  return a
+}
+putText()
