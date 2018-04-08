0e3c7511471bbbad11ee0a8bd00bffc7b37570e1
bzchacal <bzchacal@gmail.com>
2018-04-08 23:32:43 +0100

    correct Dom function

--- Project01/app.js
+++ Project01/app.js
@@ -3,7 +3,5 @@ function putText () {
   // document.getElementById("test").innerHTML = 'HI nizar'
   document.querySelector('.calss_').innerHTML = 'hello calss'
   document.querySelector('#id_').innerHTML = 'hello idd'
-  let a = 5
-  return a
 }
-putText()
+putText
