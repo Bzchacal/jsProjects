0d24ecc76fce1a7a36ac32a1a9e896e0bc1ac9b0
BENZAIM Nizar (SAFRAN AIRCRAFT ENGINES SERVICES MOROCCO) <sk21227@snm.snecma>
2018-04-10 17:05:38 +0100

    M.Collapsible  working

--- Project01/js/app.js
+++ Project01/js/app.js
@@ -1,4 +1,11 @@
 
+// import 'js/materialize.min.js'
+
+var elem = document.querySelector('.collapsible')
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+})
+
 function putText () {
   // document.getElementById("test").innerHTML = 'HI nizar'
   document.querySelector('.calss_').innerHTML = 'hello calss'
